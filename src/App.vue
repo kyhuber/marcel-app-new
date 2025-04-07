2aeac4aa3c9b7efbdf777ea5b9abf8bd2dfa6d9d
Kyle Huber <kyhuber@gmail.com>
2025-04-07 11:41:41 -0700

    Remove logout button

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
   <div id="app">
-    <nav v-if="isAuthenticated">
-      <router-link to="/dashboard">Dashboard</router-link>
-      <button @click="logout">Logout</button>
-    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { auth } from './firebase'
-
 export default {
-  data() {
-    return {
-      isAuthenticated: false
-    }
-  },
-  created() {
-    auth.onAuthStateChanged(user => {
-      this.isAuthenticated = !!user
-    })
-  },
-  methods: {
-    async logout() {
-      try {
-        await auth.signOut()
-        this.$router.push('/')
-      } catch (error) {
-        console.error('Logout error', error)
-      }
-    }
-  }
+  name: 'App'
 }
 </script>
+
+<style>
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
